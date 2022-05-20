14e64c04bf6d5d1a7c4e0407a6777231425a2a0f
Kunpeng <dcsrenk@nus.edu.sg>
2022-05-20 17:49:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const ProofUtil = require("eth-util-lite"); // maybe remove this in future versi
 
 module.exports = { GetAndVerify, GetProof, VerifyProof, ProofUtil };
 
-class MyCommand extends commander.Command {
-  createCommand(name) {
-    const cmd = new MyCommand(name);
-    cmd.option("-d,--debug", "output options");
-    return cmd;
-  }
-}
+// class MyCommand extends commander.Command {
+//   createCommand(name) {
+//     const cmd = new MyCommand(name);
+//     cmd.option("-d,--debug", "output options");
+//     return cmd;
+//   }
+// }
 
-const program = new MyCommand();
-program
-  .command("proof")
-  .option("-u --url <url>", "eth server url", "http://127.0.0.1:8545")
-  .option("--hash <tx_hash>", "tx hash")
-  .option("--index <log_index>", "event log index")
-  .action((cmd) => {
-    let getProof = new GetProof(cmd.url);
-    getProof.receiptProof(cmd.hash, cmd.index).then((tx) => {
-      console.log(tx);
-    });
-  });
+// const program = new MyCommand();
+// program
+//   .command("proof")
+//   .option("-u --url <url>", "eth server url", "http://127.0.0.1:8545")
+//   .option("--hash <tx_hash>", "tx hash")
+//   .option("--index <log_index>", "event log index")
+//   .action((cmd) => {
+//     let getProof = new GetProof(cmd.url);
+//     getProof.receiptProof(cmd.hash, cmd.index).then((tx) => {
+//       console.log(tx);
+//     });
+//   });
 
-program.parse();
+// program.parse();
